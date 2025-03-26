74c9bff93d9b20d24eb318868e3481ebdb6dd83e
Andrii Miroshnyk <55330163+mirikKun@users.noreply.github.com>
2025-03-26 21:38:05 +0200

    StateMachine fix

---
+++ Assets/Project/Scripts/AdvancedStateMachine/StateConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Project.Scripts.AdvancedStateMachine
+{
+    public struct StateConfiguration
+    {
+        public IState State;
+        public List<TransitionConfiguration> Transitions;
+    }
+}
---
+++ Assets/Project/Scripts/AdvancedStateMachine/TransitionConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Project.Scripts.AdvancedStateMachine
+{
+    public class TransitionConfiguration
+    {
+        public Type FromState;
+        public Type ToState;
+        public Func<bool> Condition;
+        public TransitionConfiguration(Type fromState, Type toState, Func<bool> condition)
+        {
+            FromState = fromState;
+            ToState = toState;
+            Condition = condition;
+        }
+        public static TransitionConfiguration GetConfiguration<TFrom,TTo>(Func<bool> condition) where TFrom:IState where TTo:IState
+        {
+            return new TransitionConfiguration(typeof(TFrom),typeof(TTo),condition);
+        }
+        public static TransitionConfiguration GetConfiguration<TFrom>(IState toState,Func<bool> condition,Func<bool> extraCondition) where TFrom:IState 
+        {
+            return new TransitionConfiguration(typeof(TFrom), toState.GetType(), 
+                () => condition() && extraCondition());
+        }
+        public static TransitionConfiguration GetConfiguration<TTo>(IState fromState,Func<bool> condition) where TTo:IState 
+        {
+            return new TransitionConfiguration( fromState.GetType(),typeof(TTo), condition);
+        }
+        public static TransitionConfiguration GetConfiguration<TFrom,TTo>(Func<bool> condition,float possibility) where TFrom:IState where TTo:IState
+        {
+            return new TransitionConfiguration(typeof(TFrom), typeof(TTo), 
+                () => condition() && UnityEngine.Random.value >= possibility);
+
+        }
+    }
+}
